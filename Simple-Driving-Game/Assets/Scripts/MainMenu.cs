455d541f998aeb2519ba00f743b65649a422926c
myxsan <yksl3461@gmail.com>
2022-06-12 20:54:24 +0300

    Set up energy system

--- Simple-Driving-Game/Assets/Scripts/MainMenu.cs
+++ Simple-Driving-Game/Assets/Scripts/MainMenu.cs
@@ -8,9 +8,41 @@ using System;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text highestScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
 
-    private void Start() {
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
+
+    private void Start()
+    {
         SetHighestScore();
+        SetEnergy();
+    }
+
+    private void SetEnergy()
+    {
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+        }
+
+        energyText.text = $"Play ({energy})";
     }
 
     private void SetHighestScore()
@@ -21,6 +53,17 @@ public class MainMenu : MonoBehaviour
 
     public void Play()
     {
+        if (energy < 1) {return;}
+
+        energy--;
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if(energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString()); 
+        }
+
         SceneManager.LoadScene(1);
     }
 }
